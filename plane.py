62b3d3e85b56be5ad4bda9557dde5c0d60373703
Chen <36320383+almbreeder@users.noreply.github.com>
2020-01-10 20:27:33 +0800

    Add files via upload

---
+++ plane.py
@@ -0,0 +1,43 @@
+import pygame
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+
+class MyPlane(pygame.sprite.Sprite):
+    def __init__(self, bg_size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image1 = pygame.image.load(os.path.join(path, "material/image/hero1.png")).convert_alpha()
+        self.rect = self.image1.get_rect()  # 限定矩形
+        self.image2 = pygame.image.load(os.path.join(path, "material/image/hero2.png")).convert_alpha()
+        self.rect = self.image2.get_rect()  # 限定矩形
+        self.width, self.height = bg_size[0], bg_size[1]
+        self.rect.left = (self.width - self.rect.width) // 2
+        # 高度
+        self.rect.top = self.height - self.rect.height - 60
+        self.speed = 10
+
+    def moveUp(self):
+        if self.rect.top > 0:
+            self.rect.top -= self.speed
+        else:
+            self.rect.top = 0
+
+    def moveDown(self):
+        if self.rect.bottom < self.height:
+            self.rect.top += self.speed
+        else:
+            self.rect.bottom = self.height - 60
+
+    def moveLeft(self):
+        if self.rect.left > 0:
+            self.rect.left -= self.speed
+        else:
+            self.rect.left = 0
+
+    def moveRight(self):
+        if self.rect.right < self.width:
+            self.rect.left += self.speed
+        else:
+            self.rect.right = self.width
